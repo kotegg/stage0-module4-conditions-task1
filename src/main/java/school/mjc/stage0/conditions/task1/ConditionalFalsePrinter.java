5a5f4c30511e20a4d5b296e53918de36d9d4665f
kotegg <lolpol.kek123@gmail.com>
2023-07-18 15:36:12 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (3 > 5) System.out.println("3 > 5");
+        else System.out.println("false");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if(5 > 3) System.out.println("true");
     }
 }
